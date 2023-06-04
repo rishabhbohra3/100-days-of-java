48957a08dd387813f142ce9a444f50ffa17a553b
Rishabh Bohra <rishabhjain.bohra@gmail.com>
2023-06-05 05:06:17 +0530

    Java 100 days challenge

---
+++ src/main/java/com/abhyasa/java/challenge/JavaConceptApplication.java
@@ -0,0 +1,13 @@
+package com.abhyasa.java.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaConceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaConceptApplication.class, args);
+	}
+
+}
